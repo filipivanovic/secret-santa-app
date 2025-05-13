0ed01482b88b4dc1a94c9b721d7ecb45b9aac4cf
filipivanovic <filip_ivanovic@hotmail.com>
2025-05-13 22:35:24 +0200

    Add components and responsive styles for Secret Santa app
    Introduced `ParticipantForm`, `ParticipantList`, and `AssignmentsDisplay` components to structure the main app functionality. Updated CSS to include responsive styling for improved usability on smaller screens and refined existing styles for better layout and design consistency.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
+const participants = [
+  { id: '1', name: 'Clark' },
+  { id: '2', name: 'Diana' },
+  { id: '3', name: 'Bruce' },
+  { id: '4', name: 'Barry' },
+  { id: '5', name: 'Lois' }
+]
+
+const Button = () => {
+  return <button>Add</button>
+}
+
 const App = () => {
   return (
     <div className="app">
       <h1>🎅 Secret Santa Planner</h1>
+      <ParticipantForm />
+      <ParticipantList />
+      {/*<Button>🎁 Assign Gifts</Button>*/}
+      <AssignmentsDisplay />
     </div>
   )
+}
+
+const ParticipantForm = () => {
+  return (
+    <form>
+      <label>👤 Participant Name: </label>
+      <input type="text" placeholder="Name" />
+      <Button>Add</Button>
+    </form>
+  )
+}
+
+const ParticipantList = () => {
+  return (
+    <ul>
+      {participants.map(participant => (
+        <li key={participant.id}>{participant.name}</li>
+      ))}
+    </ul>
+  )
+}
+
+const AssignmentsDisplay = () => {
+  return <div className="assignment">Assignments</div>
 }
 
 export default App
